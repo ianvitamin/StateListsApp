75546f4bb0ed14c08ad2b8cd6394992ea7326970
ian <ianvitamin@gmail.com>
2020-10-27 22:10:29 -0700

    changed the states table view controller

--- StateListsApp/StatesTableViewController.swift
+++ StateListsApp/StatesTableViewController.swift
@@ -1,12 +1,27 @@
 import UIKit
 
-class StatesTableViewController: UIViewController {
+class StatesTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StatesCell", for: indexPath)
+        print("cell \(indexPath) generated")
+        
+        return cell
+    }
+    
 
 }
 
